4efcda04637586adcdb1fe6eaf99987ba5e843f7
dimaderevanko <developerDD@ukr.net>
2022-03-18 17:20:35 +0200

    Initial Commit

---
+++ SpaceQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceQuiz
+//
+//  Created by Дима Деревянко on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
