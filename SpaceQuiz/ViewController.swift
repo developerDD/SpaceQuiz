f30a457b18dd1880597b518a753c7c09f18ea334
dimaderevanko <developerDD@ukr.net>
2022-03-18 23:39:26 +0200

    добавил логику на основной вюшке

--- SpaceQuiz/ViewController.swift
+++ SpaceQuiz/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lableTop: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var nameButton: UIButton!
+    //    список планет
+    let elementList = ["Місяць (Луна)", "Земля","Меркурій", "Юпітер", "Венера", "Сонце", "ОК"]
+    var cerrentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view.
     }
-// добавитьь код
-
+// функція оновленяя
+    func updateElement(){
+        answerLabel.text = "?"
+        let elementName = elementList[cerrentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        
+    }
+    @IBAction func nameButton(_ sender: UIButton) {
+        answerLabel.text = elementList[cerrentElementIndex]
+    }
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+        cerrentElementIndex += 1
+        if cerrentElementIndex == elementList.count-1 {
+            
+            let image = UIImage(named: elementList[cerrentElementIndex])
+            imageView.image = image
+            lableTop.textColor = .red
+            lableTop.text = "Молодець!"
+            answerLabel.isHidden = true
+            nameButton.isHidden = true
+            nextButton.isHidden = true
+        }
+        updateElement()
+    }
+    
 }
 
