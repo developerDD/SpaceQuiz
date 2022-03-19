9a4e38b0c106ba443e3a1e22a561570f09a3f852
dimaderevanko <developerDD@ukr.net>
2022-03-19 13:00:48 +0200

    екран входа

---
+++ SpaceQuiz/EntryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EntryViewController.swift
+//  SpaceQuiz
+//
+//  Created by Дима Деревянко on 19.03.2022.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goGame(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
