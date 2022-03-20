ff469d9ba5f369e021be3f59973b0b92bee3ca74
dimaderevanko <developerDD@ukr.net>
2022-03-20 21:04:05 +0200

    подправил код визуально

--- SpaceQuiz/EntryViewController.swift
+++ SpaceQuiz/EntryViewController.swift
@@ -8,27 +8,27 @@
 import UIKit
 
 class EntryViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func goGame(_ sender: Any) {
-//        let entryMainBord = UIStoryboard(name: "Main", bundle: Bundle.main)
-//        if let entryVC = entryMainBord.instantiateInitialViewController() as? ViewController {
-//            present(entryVC, animated: true, completion: nil)
+        //        let entryMainBord = UIStoryboard(name: "Main", bundle: Bundle.main)
+        //        if let entryVC = entryMainBord.instantiateInitialViewController() as? ViewController {
+        //            present(entryVC, animated: true, completion: nil)
         dismiss(animated: true, completion: nil)
     }
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
--- SpaceQuiz/ViewController.swift
+++ SpaceQuiz/ViewController.swift
@@ -24,14 +24,14 @@ class ViewController: UIViewController {
         updateElement()
         // Do any additional setup after loading the view.
     }
-//    показ вхідного екрану
+    //    показ вхідного екрану
     override func viewDidAppear(_ animated: Bool) {
         let entryStoryBord = UIStoryboard(name: "Entry", bundle: Bundle.main)
         if let entryVC = entryStoryBord.instantiateInitialViewController() as? EntryViewController {
             present(entryVC, animated: true, completion: nil)
         }
     }
-// функція оновленяя
+    // функція оновленяя
     func updateElement(){
         answerLabel.text = "?"
         let elementName = elementList[cerrentElementIndex]
